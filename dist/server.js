a9258ccb49f65130058c7d406b92327f92f84e6b
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2023-10-29 04:23:54 +0000

    Updated websocket

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,47 @@
+// import dotenv from 'dotenv';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
+// import http from 'http';
+// import { WebSocketServer } from 'ws'; // Import WebSocketServer from the 'ws' library
+// import app from './app.js';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// // Load environment variables from the .env file located in the parent directory
+// dotenv.config({ path: path.join(__dirname, '..', '.env') });
+// const normalizePort = (val) => {
+//     const port = parseInt(val, 10);
+//     if (isNaN(port)) {
+//         return val;
+//     }
+//     if (port >= 0) {
+//         return port;
+//     }
+//     return false;
+// };
+// const port = normalizePort(process.env.PORT || '3000');
+// app.set('port', port);
+// const httpServer = http.createServer(app);
+// const wss = new WebSocketServer({ server: httpServer });
+// wss.on('connection', (ws) => {
+//     console.log({ 'A user connected': 'blabla' });
+//     ws.on('message', (message) => {
+//         console.log('Received message from client:', message);
+//     });
+//     ws.on('close', () => {
+//         console.log('A user disconnected');
+//     });
+// });
+// httpServer.listen(port, () => {
+//     console.log(`WebSocket server is running on ws://localhost:${port}`);
+// });
+// Server without web socket:
+import http from 'http';
+import app from './app.js';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import http from 'http';
-import { WebSocketServer } from 'ws'; // Import WebSocketServer from the 'ws' library
-import app from './app.js';
+import { WebSocketServer } from 'ws';
+// Get the directory name of the current module file
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // Load environment variables from the .env file located in the parent directory
@@ -20,68 +58,46 @@ const normalizePort = (val) => {
 };
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
-const httpServer = http.createServer(app);
-const wss = new WebSocketServer({ server: httpServer });
+const errorHandler = (error) => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+const server = http.createServer(app);
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+server.listen(port);
+const wss = new WebSocketServer({ server });
 wss.on('connection', (ws) => {
-    console.log({ 'A user connected': ws });
+    console.log('A new WebSocket connection opened');
     ws.on('message', (message) => {
-        console.log('Received message from client:', message);
+        console.log(`Received: ${message}`);
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
     });
     ws.on('close', () => {
-        console.log('A user disconnected');
+        console.log('A WebSocket connection closed');
     });
 });
-httpServer.listen(port, () => {
-    console.log(`WebSocket server is running on ws://localhost:${port}`);
-});
-// Server without web socket:
-// import http from 'http';
-// import app from './app.js';
-// import dotenv from 'dotenv';
-// import { fileURLToPath } from 'url';
-// import path from 'path';
-// // Get the directory name of the current module file
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// // Load environment variables from the .env file located in the parent directory
-// dotenv.config({ path: path.join(__dirname, '..', '.env') });
-// const normalizePort = (val: string) => {
-//     const port = parseInt(val, 10);
-//     if (isNaN(port)) {
-//         return val;
-//     }
-//     if (port >= 0) {
-//         return port;
-//     }
-//     return false;
-// };
-// const port = normalizePort(process.env.PORT || '3000');
-// app.set('port', port);
-// const errorHandler = (error: NodeJS.ErrnoException) => {
-//     if (error.syscall !== 'listen') {
-//         throw error;
-//     }
-//     const address = server.address();
-//     const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-//     switch (error.code) {
-//         case 'EACCES':
-//             console.error(bind + ' requires elevated privileges.');
-//             process.exit(1);
-//             break;
-//         case 'EADDRINUSE':
-//             console.error(bind + ' is already in use.');
-//             process.exit(1);
-//             break;
-//         default:
-//             throw error;
-//     }
-// };
-// const server = http.createServer(app);
-// server.on('error', errorHandler);
-// server.on('listening', () => {
-//     const address = server.address();
-//     const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-//     console.log('Listening on ' + bind);
-// });
-// server.listen(port);
 //# sourceMappingURL=server.js.map
